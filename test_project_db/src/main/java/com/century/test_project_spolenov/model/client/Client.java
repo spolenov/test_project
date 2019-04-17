9c03ff4b1495c97bfc62d31ec23ae015dc230e4e
spolenov <spolenov@gk21.ru>
2019-04-17 10:47:45 +0400

    Тестирование нового бранча: коммит в ветку master

--- test_project_db/src/main/java/com/century/test_project_spolenov/model/client/Client.java
+++ test_project_db/src/main/java/com/century/test_project_spolenov/model/client/Client.java
@@ -2,7 +2,10 @@ package com.century.test_project_spolenov.model.client;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class Client {
     private int id;
+    private BigDecimal price;
 }
